aa39bed47c6f7783a7e8c5958e3a5397169e264b
Сергей Бобров <sbobrov85@gmail.com>
2017-04-25 21:04:51 +0500

    #63 Fix File toolbar position

--- src/ru/sbobrov85/uploadfilesbutton/UploadFilesButton.java
+++ src/ru/sbobrov85/uploadfilesbutton/UploadFilesButton.java
@@ -47,7 +47,7 @@ import ru.sbobrov85.uploadfilesbutton.classes.StateHelper;
         position = 9100,
         separatorBefore = 9050
     ),
-    @ActionReference(path = "Toolbars/File", position = 3100),
+    @ActionReference(path = "Toolbars/File", position = 500),
     @ActionReference(path = "Shortcuts", name = "DO-U")
 })
 
